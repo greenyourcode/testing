46bbde2df472cfe53247e718fa3ac220fe019385
valou <jedickerz@gmail.com>
2020-06-22 13:53:04 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [NgxDatatableModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
